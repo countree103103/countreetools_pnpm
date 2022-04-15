5297f37cf2ec504a48ab57bf9772692ca33c8852
Countree <717662837@qq.com>
2022-04-15 12:15:09 +0800

    move to rollup.

---
+++ packages/countreetools_client/rollup.config.js
@@ -0,0 +1,12 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import rollupJson from "@rollup/plugin-json";
+
+export default {
+  input: 'dist/backend.js',
+  output: {
+    file: 'bundle/r_bundle.js',
+    format: 'cjs'
+  },
+  plugins: [nodeResolve(), commonjs(), rollupJson()]
+};
