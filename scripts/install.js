2395474f983e124b554c3168c58cea7cb10fc124
Countree <717662837@qq.com>
2022-04-15 22:29:15 +0800

    add postinstall script.

--- scripts/install.js
+++ scripts/install.js
@@ -6,7 +6,6 @@ const spin = ora()
 async function main(){
   try {
     spin.start("installing dependencies...");
-    execaCommandSync(`pnpm install`, {stdio:"inherit"});
     execaCommandSync(`pnpm install -F *server -F *utils`, {stdio:"inherit"});
     execaCommandSync(`pnpm install --shamefully-hoist -F *client`, {stdio:"inherit"});
     spin.succeed("installed dependencies succeed!");
