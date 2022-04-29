8ee3bed7c0b604fd8e5b106a1dcb49c99efc416a
Countree <717662837@qq.com>
2022-04-29 14:36:21 +0800

    move store.

--- packages/web_ele/src/store/index.ts
+++ packages/web_ele/src/store/index.ts
@@ -1,5 +1,53 @@
 import { createStore } from "vuex";
 
+const store = createStore({
+  state: {
+    globalStatus: "测试状态",
+    clientArr: window.clientArr,
+    cmdResult: window.cmdResult,
+    clients: {
+      verify: {
+        show: false,
+        // show: true,
+      },
+    },
+    contextMenu: {
+      // id: "",
+      show: false,
+      mousePos: {
+        screenX: 0,
+        screenY: 0,
+        clientX: 0,
+        clientY: 0,
+      },
+    },
+  },
+  getters: {
+    status(state) {
+      return state.globalStatus;
+    },
+    getSelectedClientId(state) {
+      return state.selectedClientId;
+    },
+    getClientArr(state) {
+      return state.clientArr;
+    },
+    getCmdResult(state) {
+      return state.cmdResult.data;
+    },
+  },
+  mutations: {
+    clearResult(state) {
+      state.cmdResult.data = "";
+    },
+    setGlobalStatus(state, status) {
+      state.globalStatus = status;
+    },
+  },
+  actions: {},
+  modules: {},
+});
+
 export default createStore({
   state: {},
   getters: {},
