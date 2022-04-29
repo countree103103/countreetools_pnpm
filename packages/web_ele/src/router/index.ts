08ce1921a0f21894bd1f8f835d4478c441224876
Countree <717662837@qq.com>
2022-04-29 14:50:56 +0800

    comment router

--- packages/web_ele/src/router/index.ts
+++ packages/web_ele/src/router/index.ts
@@ -18,45 +18,45 @@ const routes: Array<RouteRecordRaw> = [
   },
 ];
 
-const routes_notImple: Array<RouteRecordRaw> = [
-  {
-    path: "/ssh",
-    name: "SSH",
-    component: () => import("../views/Ssh.vue"),
-  },
-  {
-    path: "/ip",
-    name: "IP",
-    component: () => import("../views/Ip.vue"),
-  },
-  {
-    path: "/trojan/clients",
-    name: "Clients",
-    alias: "/",
-    component: () => import("../views/Trojan/Clients.vue"),
-    meta: {
-      keepAlive: true, //需要被缓存的组件
-    },
-  },
-  {
-    path: "/trojan/control/:id",
-    name: "Control",
-    component: () => import("../views/Trojan/Control.vue"),
-    props: true,
-  },
-  {
-    path: "/trojan/stream/:id",
-    name: "Stream",
-    component: () => import("../views/Trojan/Stream.vue"),
-    props: true,
-  },
-  {
-    path: "/trojan/fileexplorer",
-    name: "FileExplorer",
-    component: () => import("../views/Trojan/FileExplorer.vue"),
-    props: true,
-  },
-];
+// const routes_notImple: Array<RouteRecordRaw> = [
+//   {
+//     path: "/ssh",
+//     name: "SSH",
+//     component: () => import("../views/Ssh.vue"),
+//   },
+//   {
+//     path: "/ip",
+//     name: "IP",
+//     component: () => import("../views/Ip.vue"),
+//   },
+//   {
+//     path: "/trojan/clients",
+//     name: "Clients",
+//     alias: "/",
+//     component: () => import("../views/Trojan/Clients.vue"),
+//     meta: {
+//       keepAlive: true, //需要被缓存的组件
+//     },
+//   },
+//   {
+//     path: "/trojan/control/:id",
+//     name: "Control",
+//     component: () => import("../views/Trojan/Control.vue"),
+//     props: true,
+//   },
+//   {
+//     path: "/trojan/stream/:id",
+//     name: "Stream",
+//     component: () => import("../views/Trojan/Stream.vue"),
+//     props: true,
+//   },
+//   {
+//     path: "/trojan/fileexplorer",
+//     name: "FileExplorer",
+//     component: () => import("../views/Trojan/FileExplorer.vue"),
+//     props: true,
+//   },
+// ];
 
 const router = createRouter({
   history: createWebHashHistory(),
