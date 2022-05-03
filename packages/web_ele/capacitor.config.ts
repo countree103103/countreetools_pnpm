f6b4b7c2a91adb9aad70be7d6ab041c31aac6487
Countree <717662837@qq.com>
2022-05-04 00:24:21 +0800

    add capacitor

---
+++ packages/web_ele/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'cn.countree.tools',
+  appName: 'countreetoolsTools',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
